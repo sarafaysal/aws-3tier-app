d18fecd55348b47175626305115e9d82b4d4b877
Ubuntu <ubuntu@ip-10-0-1-214.eu-north-1.compute.internal>
2026-07-16 19:26:42 +0000

    Configure Vite for AWS Application Load Balancer

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    proxy: {
-      '/api': {
-        target: 'http://10.0.134.193:5000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+    host: '0.0.0.0',
+    allowedHosts: true
+  }
 })
